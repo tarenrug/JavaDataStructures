ea727a4d0e0f243f72e5fb1a43dc6023fa5cca83
Taren HP Laptop <taren_rug@hotmail.co.uk>
2022-04-23 22:09:32 +0100

    test

--- JavaTestingFile.java
+++ JavaTestingFile.java
@@ -1,8 +1,10 @@
 import Queue.LinkedQueue;
+import java.util.*;
 
 class JavaTestingFile { 
     public static void main (String[] param) {
-        QueueTesting();
+        // QueueTesting();
+        dequeTesting();
         System.exit(0);
     }
     public static void QueueTesting() {
@@ -15,5 +17,37 @@ class JavaTestingFile {
         System.out.println(Taren.peek());
         System.out.println(Taren.poll());
         System.out.println(Taren.peek());
+    }
+
+    public static void dequeTesting() {
+        Deque deque = new ArrayDeque<>();
+        deque.addFirst(2);
+        deque.addFirst(2);
+        deque.addFirst(2);
+        deque.addFirst(2);
+        deque.addFirst(5);
+        deque.addLast(34);
+        deque.addFirst(55);
+        deque.addLast(34);
+        deque.addLast(34);
+        deque.addFirst(55);
+
+        HashMap hashMap = new HashMap<Integer, Integer>();
+
+        for (Object i : deque) {
+            if (hashMap.containsKey(i)) { 
+                hashMap.put(i, (Integer) hashMap.get(i) + 1);
+            } else {
+                hashMap.put(i, 1);
+            }
+        }
+
+        System.out.println(hashMap);
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(1);
+        arrayList.add(3);
+
+        System.out.println(arrayList.stream().max(Integer::compare).get());
+
     }
 } 
